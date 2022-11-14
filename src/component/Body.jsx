3df1a79f02a19602fe2dbbc18144dde623c58e2f
elbrl <elbrl@iMAC2021-05s-iMac.local>
2022-11-14 19:20:47 +0800

    neg yum

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -2,28 +2,70 @@ import "./Body.css";
 import { Link } from "react-router-dom";
 export const Body = () => {
   return (
-    <div className="Body1">
-      <div className="teamwork">
-        <div className="sda">
-          <div className="title">
-            <span className="Garchig">
-              Your Hub for <br /> teamwork <br />
-            </span>{" "}
-            <br />
-            Give everyone you work with—inside and outside your <br /> company—a
-            more productive way to stay in sync. <br /> Respond faster with
-            emoji, keep conversations focused <br /> in channels, and simplify
-            all your communication into <br /> one place. <br />
-            <Link className="li" to="">
-              Learn more
-            </Link>
-            <img src="sum.svg" alt="" />
+    <div>
+      <div className="Body1">
+        <div className="teamwork">
+          <div className="sda">
+            <div className="title">
+              <span className="Garchig">
+                Your Hub for <br /> teamwork <br />
+              </span>{" "}
+              <br />
+              Give everyone you work with—inside and outside your <br />{" "}
+              company—a more productive way to stay in sync. <br /> Respond
+              faster with emoji, keep conversations focused <br /> in channels,
+              and simplify all your communication into <br /> one place. <br />
+              <Link className="li" to="">
+                Learn more
+              </Link>
+              <img src="sum.svg" alt="" />
+            </div>
+          </div>
+          <img className="zurg1" src="Meeting.svg" alt="" />
+        </div>
+      </div>
+      <div className="Body2">
+        <div className="teamwork">
+          <img className="zurg2" src="Egch.svg" alt="" />
+          <div className="sda">
+            <div className="title">
+              <span className="Garchig">
+                Simple task <br /> management <br />
+              </span>{" "}
+              <br />
+              Give everyone you work with—inside and outside your <br />{" "}
+              company—a more productive way to stay in sync. <br /> Respond
+              faster with emoji, keep conversations focused <br /> in channels,
+              and simplify all your communication into <br /> one place. <br />
+              <Link className="li" to="">
+                Learn more
+              </Link>
+              <img src="sum.svg" alt="" />
+            </div>
           </div>
         </div>
-        <img src="Meeting.svg" alt="" />
       </div>
-      {/* ////////////// */}
-      <div></div>
+      <div className="Body3">
+        <div className="teamwork">
+          <div className="sda">
+            <div className="title">
+              <span className="Garchig">
+                Scheduling that <br /> actually works teamwork <br />
+              </span>{" "}
+              <br />
+              Give everyone you work with—inside and outside your <br />{" "}
+              company—a more productive way to stay in sync. <br /> Respond
+              faster with emoji, keep conversations focused <br /> in channels,
+              and simplify all your communication into <br /> one place. <br />
+              <Link className="li" to="">
+                Learn more
+              </Link>
+              <img src="sum.svg" alt="" />
+            </div>
+          </div>
+          <img className="zurg3" src="Ah.svg" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
