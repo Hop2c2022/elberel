3b9b6c4d26b8aaeffdd00ca11752473551c7d651
unknown <s20c034b@nmct.edu.mn>
2022-11-25 12:38:41 +0800

    ghuk

--- netflix/src/component/Sign1.jsx
+++ netflix/src/component/Sign1.jsx
@@ -1,4 +1,22 @@
 import "./Sign.css";
 export const Sign1 = () => {
-  return <div>pppppp</div>;
+  return (
+    <div className="tom tomm">
+      <div
+        style={{
+          height: "85px",
+          width: "100vh",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <img
+          src="./Logo.svg"
+          alt=""
+          style={{ height: "36px", width: "133px", backgroundSize: "cover" }}
+        />
+      </div>
+      <div></div>
+    </div>
+  );
 };
